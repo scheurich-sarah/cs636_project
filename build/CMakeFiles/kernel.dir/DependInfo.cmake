a98b9b39f0af7a5be17e785d42358109f4581512
Sarah C  Scheurich <sarah.scheurich@richmond.edu>
2021-12-07 13:02:49 -0500

    not working

---
+++ build/CMakeFiles/kernel.dir/DependInfo.cmake
@@ -0,0 +1,28 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/scsche/cs636_Big_Data/cs636_project/kernel/kernel.cpp" "/home/scsche/cs636_Big_Data/cs636_project/build/CMakeFiles/kernel.dir/kernel.cpp.o"
+  "/home/scsche/cs636_Big_Data/cs636_project/kernel/kernel_pybind.cpp" "/home/scsche/cs636_Big_Data/cs636_project/build/CMakeFiles/kernel.dir/kernel_pybind.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "kernel_EXPORTS"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/home/scsche/cs636_Big_Data/cs636_project/kernel/pybind11/include"
+  "/usr/include/python3.8"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
